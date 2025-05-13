070a8af6d7ad0127fe9ae92eb14dce25c26e527e
k11b0u <hiroaki.murayama0428@icloud.com>
2025-05-13 10:55:33 +0900

    Update server.py

--- server.py
+++ server.py
@@ -45,6 +45,7 @@ def callback():
     requests.put("https://api.spotify.com/v1/me/player/play", headers={
         "Authorization": f"Bearer {token}"
     },json={"context_uri": "spotify:playlist:37i9dQZF1DXcBWIGoYBM5M"}
+                )
 
 
     return "✅ Spotifyに再生リクエストを送りました！"
